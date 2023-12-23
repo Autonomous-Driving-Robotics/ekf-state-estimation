86f9c4a9fd166f233f889ad6c4eac8e3ef00eac1
Dikshant Gupta <dikshant2210@gmail.com>
2023-12-23 13:47:37 +0000

    first implementation basic kalman filter

--- src/slam/src/ekf_filter.cpp
+++ src/slam/src/ekf_filter.cpp
@@ -3,15 +3,43 @@
 
 #include "ekf_filter.hpp"
 
+ExtendedKalmanFilter::ExtendedKalmanFilter()
+{
+    // Set state transition matrix
+    state_transition_matrix(0, 2) = delta_t;
+    state_transition_matrix(0, 4) = 0.5 * delta_t * delta_t;
+    state_transition_matrix(1, 4) = delta_t;
+    state_transition_matrix(1, 5) = 0.5 * delta_t * delta_t;
+    state_transition_matrix(2, 4) = delta_t;
+    state_transition_matrix(3, 5) = delta_t;
+
+    // Set control input
+    control_input(4, 0) = 0.1;
+    control_input(5, 0) = 0.1;
+
+    // Set measurement matrix
+    measurement_matrix(0, 0) = 1;
+    measurement_matrix(1, 1) = 1;
+}
+
 void ExtendedKalmanFilter::Predict()
 {
-    state_vector = state_transition_matrix * state_vector + control_input_matrix * control_input +
-                   process_noise;
+    state_vector = state_transition_matrix * state_vector + control_input_matrix * control_input;
+    prediction_covariance =
+        state_transition_matrix * prediction_covariance * state_transition_matrix.transpose() +
+        process_noise;
 }
 
 void ExtendedKalmanFilter::Update()
 {
-    measurement = measurement_matrix * measurement + measurement_noise;
+    MatrixXd measurement_state = measurement_matrix * state_vector;
+    MatrixXd innovation_covariance{measurement_noise + measurement_matrix * prediction_covariance *
+                                                           measurement_matrix.transpose()};
+    kalman_gain =
+        prediction_covariance * measurement_matrix.transpose() * innovation_covariance.inverse();
+    state_vector = state_vector + kalman_gain * (measurement - measurement_state);
+    prediction_covariance =
+        prediction_covariance - kalman_gain * innovation_covariance * kalman_gain.transpose();
 }
 
 void ExtendedKalmanFilter::SetMeasurement(double x, double y)
--- src/slam/src/ekf_filter.hpp
+++ src/slam/src/ekf_filter.hpp
@@ -8,7 +8,7 @@ typedef Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> MatrixXd;
 class ExtendedKalmanFilter
 {
   public:
-    ExtendedKalmanFilter() {}
+    ExtendedKalmanFilter();
     void Predict();
     void Update();
     void SetMeasurement(double x, double y);
@@ -19,15 +19,17 @@ class ExtendedKalmanFilter
 
   private:
     double delta_t{};
-    MatrixXd state_vector{4, 1};  // x, y, vx, vy
-    MatrixXd state_transition_matrix{4, 4};
-    MatrixXd control_input{4, 1};  //
-    MatrixXd control_input_matrix{4, 4};
-    MatrixXd process_noise{4, 1};
+    MatrixXd state_vector{MatrixXd::Zero(6, 1)};  // x, y, vx, vy, ax, ay
+    MatrixXd state_transition_matrix{MatrixXd::Identity(6, 6)};
+    MatrixXd control_input{MatrixXd::Zero(6, 1)};  // 0, 0, 0, 0, ax, ay
+    MatrixXd control_input_matrix{MatrixXd::Identity(6, 6)};
+    MatrixXd process_noise{MatrixXd::Identity(6, 6)};
+    MatrixXd prediction_covariance{MatrixXd::Identity(6, 6) * 0.01};
 
-    MatrixXd measurement{2, 1};
-    MatrixXd measurement_matrix{2, 4};
-    MatrixXd measurement_noise{2, 1};
+    MatrixXd measurement{MatrixXd::Zero(2, 1)};  // x, y
+    MatrixXd measurement_matrix{MatrixXd::Zero(2, 6)};
+    MatrixXd measurement_noise{MatrixXd::Identity(2, 2)};
+    MatrixXd kalman_gain{6, 2};
 };
 
 #endif
